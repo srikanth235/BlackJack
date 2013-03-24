c15dc10cbe596dd984d5f6d9c110cf5ed5754665
srikanth <srikanth@srikanth-Lenovo-Z580>
2013-03-24 01:39:50 -0500

    Adding creation of games implementation.

--- Blackjack/BlackJack.py
+++ Blackjack/BlackJack.py
@@ -2,15 +2,18 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import ndb
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
 import random
+import os
 
 class Game(ndb.Model):
-    name = db.StringProperty(required=True)
-    identifier = db.IntegerProperty(required=True)
-    players_max = db.IntegerProperty(required=True)
-    players_current = db.IntegerProperty(required=True)
-    deck = db.StringListProperty(required=True)
-    cards_dealt = db.StringListProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    identifier = ndb.IntegerProperty()
+    players_max = ndb.IntegerProperty()
+    players_current = ndb.IntegerProperty()
+    deck = ndb.StringProperty(repeated=True)
+    cards_dealt = ndb.StringProperty(repeated=True)
 
 class Player(ndb.Model):
     name = db.StringProperty(required=True)
@@ -28,19 +31,22 @@ class Game_Status():
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        out.write(template.render(path, {'rows': rows, 'desc': desc, 'status': status_ships}))
+        self.response.headers['Content-Type'] = 'text/html'
+        rows = Game.query()
+        path = os.path.join(os.path.dirname(__file__), 'html/active_games.html')
+        self.response.out.write(template.render(path, {'rows': rows}))
     def post(self):
-        game = Game(name=self.request.get("name"),
-                    players_max = self.request.get("players_max"),
-                    identifier = random.randint(0, 1000000)
+        game = Game(name = self.request.get("name"),
+                    players_max = int(self.request.get("players_max")),
+                    identifier = random.randint(0, 1000000),
                     players_current = 0,
                     deck = [],
                     cards_dealt = [])
         game.put()
-        self.response.out.write("Game created successfully")
+        self.response.out.write(
+                     "<html><body><h2>"
+                     + self.request.get("name")
+                     + " created successfully</body></html>")
 
 app = webapp.WSGIApplication(
           [('/games', MainPage)],
